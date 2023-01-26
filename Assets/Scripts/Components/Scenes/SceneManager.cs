9c964ef621b38cb02d8eb1c033b75f8393ed0af0
Qoqqi <sazykin.georgii@gmail.com>
2023-01-27 02:46:35 +0300

    Добавлен переход при переключении экранов

--- Assets/Scripts/Components/Scenes/SceneManager.cs
+++ Assets/Scripts/Components/Scenes/SceneManager.cs
@@ -7,16 +7,32 @@ namespace Components.Scenes {
     public class SceneManager : MonoBehaviour {
 
         public static string CurrentSceneName => UnitySceneManager.GetActiveScene().name;
+
+        [SerializeField] private ScreenFader screenFader;
+
+        [SerializeField] private Camera fallbackCamera;
+
+        [SerializeField] private float fadeDuration = 1;
+        
+        private ScreenScene CurrentScreen;
         
         private void Awake() {
+            fallbackCamera.enabled = false;
         }
 
         public async Task LoadLevel() {
-            await LoadSceneByName("Scenes/Screens/LevelScene");
+            await SwitchScreen("Scenes/Screens/LevelScreen");
         }
 
         public async Task LoadMainMenu() {
-            await LoadSceneByName("Scenes/Screens/MainMenuScene");
+            await SwitchScreen("Scenes/Screens/MainMenuScreen");
+        }
+
+        public async Task SwitchScreen(string sceneName) {
+            await FadeOut();
+            await UnloadCurrentScreen();
+            await LoadSceneByName(sceneName);
+            await FadeIn();
         }
 
         public async Task LoadSceneByName(string sceneName) {
@@ -25,6 +41,28 @@ namespace Components.Scenes {
             while (!asyncOperation.isDone) {
                 await Task.Yield();
             }
+
+            CurrentScreen = FindObjectOfType<ScreenScene>();
+        }
+
+        private async Task UnloadCurrentScreen() {
+            if (!CurrentScreen) {
+                return;
+            }
+            
+            CurrentScreen.Unload();
+            
+            await Task.CompletedTask;
+        }
+
+        private async Task FadeOut() {
+            await screenFader.FadeOut(fadeDuration);
+            fallbackCamera.enabled = true;
+        }
+
+        private async Task FadeIn() {
+            fallbackCamera.enabled = false;
+            await screenFader.FadeIn(fadeDuration);
         }
 
         public static void LoadSystemScene() {
---
+++ Assets/Scripts/Components/Scenes/ScreenFader.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Components.Scenes {
+    public class ScreenFader : MonoBehaviour {
+
+        [SerializeField] private Canvas canvas;
+
+        [SerializeField] private Image overlayImage;
+
+        public void Awake() {
+            canvas.enabled = false;
+        }
+
+        public async Task FadeOut(float duration) {
+            canvas.enabled = true;
+            
+            await FadeAsync(0, 1, duration);
+        }
+
+        public async Task FadeIn(float duration) {
+            await FadeAsync(1, 0, duration);
+            
+            canvas.enabled = false;
+        }
+
+        private async Task FadeAsync(float from, float to, float duration) {
+            StartCoroutine(Fade(from, to, duration));
+            
+            await DelaySeconds(duration);
+        }
+
+        private IEnumerator Fade(float from, float to, float duration) {
+            var elapsed = 0f;
+
+            while (elapsed < duration) {
+                elapsed += Time.deltaTime;
+                SetOpacity(from + (to - from) * elapsed);
+                yield return null;
+            }
+        }
+
+        private void SetOpacity(float opacity) {
+            var color = overlayImage.color;
+
+            color.a = opacity;
+
+            overlayImage.color = color;
+        }
+
+        private static async Task DelaySeconds(float duration) {
+            await Task.Delay(Mathf.FloorToInt(duration * 1000));
+        }
+    }
+}
