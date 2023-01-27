400ecf571b2d9a4997f62f01e669aee479e32bfd
Qoqqi <sazykin.georgii@gmail.com>
2023-01-27 16:04:24 +0300

    Добавлен тестовый экран смерти

---
+++ Assets/Scripts/Components/Scenes/DeathScreenScene.cs
@@ -0,0 +1,8 @@
+﻿namespace Components.Scenes {
+    public class DeathScreenScene : ScreenScene {
+        
+        public async void OnClick() {
+            await Game.Instance.SceneManager.LoadLocationMap();
+        }
+    }
+}
