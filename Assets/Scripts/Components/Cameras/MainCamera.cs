6c07c2a600e6e3a999d39703702b4fe4c0a38ed1
Qoqqi <sazykin.georgii@gmail.com>
2023-02-03 08:28:45 +0300

    Включено Pixel-perfect отображение

--- Assets/Scripts/Components/Cameras/MainCamera.cs
+++ Assets/Scripts/Components/Cameras/MainCamera.cs
@@ -8,6 +8,8 @@ namespace Components.Cameras {
 
         public Camera Camera { get; private set; }
 
+        [SerializeField] private float paddingUnits = 4; 
+        
         private Board board;
 
         private Vector3 Position {
@@ -26,6 +28,10 @@ namespace Components.Cameras {
             Camera = GetComponent<Camera>();
         }
 
+        private void Update() {
+            Setup();
+        }
+
         private void OnEnable() {
             GameEvents.Instance.On<LevelLoadedEvent>(OnLevelLoaded);
         }
@@ -41,19 +47,26 @@ namespace Components.Cameras {
         }
 
         private void Setup() {
-            if (board) {
-                Position = board.CenterPosition;
-                SetSize(board.Height);
-                Adjust(board.Height);
+            if (!board) {
+                return;
             }
+
+            var center = board.CenterPosition;
+            var minX = Mathf.Min(board.Bounds.xMin + paddingUnits, center.x);
+            var minY = Mathf.Min(board.Bounds.yMin + paddingUnits, center.y);
+            var maxX = Mathf.Max(board.Bounds.xMax - paddingUnits, center.x);
+            var maxY = Mathf.Max(board.Bounds.yMax - paddingUnits, center.y);
+
+            var position = board.Player.transform.position;
+
+            position.x = Mathf.Clamp(position.x, minX, maxX);
+            position.y = Mathf.Clamp(position.y, minY, maxY);
+            
+            Position = position + GetCameraOffset();
         }
 
-        private void SetSize(int boardHeight) {
-            Camera.orthographicSize = boardHeight / 2f * 1.6f;
-        }
-
-        private void Adjust(int boardHeight) {
-            transform.position += Vector3.down * (boardHeight * 0.25f);
+        private Vector3 GetCameraOffset() {
+            return Vector3.down * 1.5f;
         }
     }
 }
